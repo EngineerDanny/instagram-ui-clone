d596a03855c0c8865ecee218b970b1e033b730b0
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-03-07 12:18:26 +0000

    Header UI updates

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,8 +14,8 @@ const Header = () => {
   ];
 
   return (
-    <div>
-      <div className=" mt-4 pb-2 flex flex-row overflow-x-visible overflow-scroll scroll-smooth   scrollbar-hide space-x-3 border-b-2 ">
+    <div className=" m-3 border border-gray-300 bg-white rounded-lg">
+      <div className=" mt-4 pb-2 flex flex-row overflow-x-visible overflow-scroll scroll-smooth   scrollbar-hide space-x-3 ">
         {list.map((item) => (
           <div className=" first:ml-4 flex flex-col items-center">
             <div className=" h-[70px] w-[70px]  rounded-full border-pink-600  border-2 bg-transparent flex flex-col items-center justify-center">
