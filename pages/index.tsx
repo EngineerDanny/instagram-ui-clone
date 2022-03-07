4625ee9a1d10510f9bef1ff9e761fff11e6a5b64
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-03-07 12:19:14 +0000

    index page UI update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,26 @@ import Image from "next/image";
 import FeedTile from "../components/FeedTile";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import Sidebar from "../components/Sidebar";
 
 const Home: NextPage = () => {
   return (
     <div>
       <NavBar />
-      <Header/>
-      <FeedTile/>
+      <main className=" flex flex-row justify-center w-full">
+        <div className=" w-full md:max-w-2xl">
+          <Header />
+          <FeedTile />
+          <FeedTile />
+          <FeedTile />
+          <FeedTile />
+        </div>
+
+        <div className="hidden lg:block w-[400px] ">
+          {/* Instagram Sidebar  */}
+          <Sidebar />
+        </div>
+      </main>
     </div>
   );
 };
