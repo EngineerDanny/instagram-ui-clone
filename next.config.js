f563d328fd11c172fa6d0ad6126f046ad3eba6dd
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-03-07 12:17:49 +0000

    add new image domains

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["www.instagram.com", "api.themoviedb.org"],
+    domains: [
+      "www.instagram.com",
+      "api.themoviedb.org",
+      "source.unsplash.com",
+      "picsum.photos",
+    ],
   },
   reactStrictMode: true,
 };
